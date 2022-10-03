8fdbc3a77a0eeec8a0c8d7ab65a678df2606a639
Jose Luis Pintado <joseluispintado666@gmail.com>
2022-10-03 12:40:47 -0300

    instalacion de librerias react, carpetas y archivos iniciales (funcionando)

---
+++ src/components/Location/Location.jsx
@@ -0,0 +1,5 @@
+// import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+// import { FavoritesContext } from '../../context/FavoritesContext';
+import './Location.css';
